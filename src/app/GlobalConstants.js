fc9abd492fe174df2523d4e744ef1cf92320b978
KellenSmith <kellensmith407@gmail.com>
2025-02-09 16:15:19 +0100

    Created globalconstants file

---
+++ src/app/GlobalConstants.js
@@ -0,0 +1,15 @@
+const GlobalConstants = {
+    // User
+    ID: 'id',
+    FIRST_NAME: 'firstName',
+    SURNAME: 'surName',
+    NICKNAME: 'nickname',
+    EMAIL: 'email',
+    PHONE: 'phone',
+    ROLE: 'role',
+    STATUS: 'status',
+    MEMBER_SINCE: 'memberSince',
+    MEMBERSHIP_RENEWED_AT: 'membershipRenewedAt'
+}
+
+export default GlobalConstants
