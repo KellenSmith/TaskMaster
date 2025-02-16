9c6f4ccdfb8612e54335eeb0f16de0fd64c8874c
KellenSmith <kellensmith407@gmail.com>
2025-02-16 17:06:52 +0100

    Moved loginpage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,32 @@
-import LoginForm from "./login-form";
+"use client";
 
-const LoginPage: React.FC = () => {
-  return <LoginForm />;
+import GlobalConstants from "../GlobalConstants";
+import Form, { FormActionState } from "../ui/form/Form";
+import { login } from "../lib/actions";
+import { useUserContext } from "../context/UserContext";
+import { redirect } from "next/navigation";
+
+const LoginForm: React.FC = () => {
+  const { setUser } = useUserContext();
+
+  const logInAndUpdateContext = async (
+    currentActionState: FormActionState,
+    formData: FormData,
+  ) => {
+    const logInActionState = await login(currentActionState, formData);
+    if (logInActionState.status === 200) {
+      setUser(JSON.parse(logInActionState.result));
+      redirect("/");
+    }
+    return logInActionState;
+  };
+  return (
+    <Form
+      name={GlobalConstants.LOGIN}
+      buttonLabel={GlobalConstants.LOGIN}
+      action={logInAndUpdateContext}
+    />
+  );
 };
 
-export default LoginPage;
+export default LoginForm;
