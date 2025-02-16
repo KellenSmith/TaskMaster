9afc906571d899698a53ea5ce37fb409e5ac39b2
KellenSmith <kellensmith407@gmail.com>
2025-02-16 18:40:07 +0100

    Clarified middleware logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,35 +5,29 @@ import GlobalConstants from "./app/GlobalConstants";
 import { decryptJWT } from "./app/lib/auth/auth";
 import { NextURL } from "next/dist/server/web/next-url";
 import { routes } from "./app/lib/definitions";
+import { routeHasPrivacyStatus } from "./app/lib/utils";
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$|.*\\.ico$).*)"],
 };
 
-const routeHasPrivacyStatus = (
-  reqPath: string,
-  privacyStatus: string,
-): boolean => {
-  for (let route of routes[privacyStatus]) {
-    if (reqPath.startsWith(`/${route}`)) return true;
-  }
-  return false;
-};
-
 export default async function middleware(req: NextRequest) {
   const reqPath = req.nextUrl.pathname;
   const redirectUrl = new NextURL(req.nextUrl);
+  redirectUrl.pathname = `/${GlobalConstants.LOGIN}`;
   const loggedInUser = await decryptJWT();
 
-  redirectUrl.pathname = `/${GlobalConstants.LOGIN}`;
-  // Redirect to login from non-public pages if the user is not logged in
-  if (!routeHasPrivacyStatus(reqPath, GlobalConstants.PUBLIC) && !loggedInUser)
+  // User not logged in
+  if (!loggedInUser) {
+    if (routeHasPrivacyStatus(reqPath, GlobalConstants.PUBLIC))
+      return NextResponse.next();
+    // Redirect to login from non-public pages if the user is not logged in
     return NextResponse.redirect(redirectUrl);
-
+  }
   redirectUrl.pathname = "/";
   // Redirect logged in users from login to home
-  if (loggedInUser && reqPath === `/${GlobalConstants.LOGIN}`)
+  if (reqPath === `/${GlobalConstants.LOGIN}`)
     return NextResponse.redirect(redirectUrl);
 
   // Redirect non-admins to home from admin pages or login
@@ -42,7 +36,6 @@ export default async function middleware(req: NextRequest) {
       loggedInUser[GlobalConstants.ROLE] === GlobalConstants.ADMIN;
     if (!userIsAdmin) return NextResponse.redirect(redirectUrl);
   }
-
   // Allow logged in users to access all private routes
   return NextResponse.next();
 }
