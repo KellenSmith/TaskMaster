59ff514face92dbf137c0c1dd43435cb22535a87
KellenSmith <51274828+KellenSmith@users.noreply.github.com>
2025-02-20 20:44:20 +0100

    Enabled deleting events (#56)

--- src/app/calendar/[event-id]/page.tsx
+++ src/app/calendar/[event-id]/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-import { getEventById, updateEvent } from "../../lib/event-actions";
-import { defaultActionState } from "../../ui/Datagrid";
-import { startTransition, useActionState, useEffect, useMemo } from "react";
-import { CircularProgress, Stack, TextField } from "@mui/material";
-import Form, { FormActionState } from "../../ui/form/Form";
+import { redirect, usePathname } from "next/navigation";
+import { deleteEvent, getEventById, updateEvent } from "../../lib/event-actions";
+import { defaultActionState as defaultDatagridActionState } from "../../ui/Datagrid";
+import { startTransition, useActionState, useEffect, useMemo, useState } from "react";
+import { Button, CircularProgress, Stack, TextField, Typography, useTheme } from "@mui/material";
+import Form, {
+    FormActionState,
+    defaultActionState as defaultFormActionState,
+} from "../../ui/form/Form";
 import GlobalConstants from "../../GlobalConstants";
 import { FieldLabels } from "../../ui/form/FieldCfg";
 
 const EventPage = () => {
+    const theme = useTheme();
     const pathname = usePathname();
     const eventId = useMemo(() => pathname.split("/").at(-1), [pathname]);
+    const [errorMsg, setErrorMsg] = useState("");
 
     const getEvent = async () => {
         return getEventById(eventId, fetchEventState);
@@ -19,7 +24,7 @@ const EventPage = () => {
 
     const [fetchEventState, fetchEventAction, isEventPending] = useActionState(
         getEvent,
-        defaultActionState,
+        defaultDatagridActionState,
     );
 
     useEffect(() => {
@@ -33,16 +38,27 @@ const EventPage = () => {
     const updateEventById = async (currentActionState: FormActionState, formData: FormData) => {
         return updateEvent(eventId, currentActionState, formData);
     };
-
+    console.log(fetchEventState.result);
     const getHostNickname = () =>
         fetchEventState.result.length > 0
             ? fetchEventState.result[0][GlobalConstants.HOST][GlobalConstants.NICKNAME]
             : "";
 
+    const deleteEventAndRedirect = async () => {
+        const deleteResult = await deleteEvent(eventId, defaultFormActionState);
+        if (deleteResult.errorMsg) return setErrorMsg(deleteResult.errorMsg);
+        // Redirect to calendar when event is deleted
+        redirect(`/${GlobalConstants.CALENDAR}`);
+    };
+
     return (
         <Stack>
             {isEventPending ? (
                 <CircularProgress />
+            ) : fetchEventState.result.length < 1 ? (
+                <Typography color={theme.palette.text.secondary}>
+                    {"Sorry, this event doesn't exist"}
+                </Typography>
             ) : (
                 <Stack>
                     <TextField
@@ -61,6 +77,10 @@ const EventPage = () => {
                                 : undefined
                         }
                     />
+                    <Typography color="error">{errorMsg}</Typography>
+                    <Button color="error" onClick={deleteEventAndRedirect}>
+                        delete
+                    </Button>
                 </Stack>
             )}
         </Stack>
