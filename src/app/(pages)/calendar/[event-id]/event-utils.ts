886f1a34b2ec2d90b77f11a3328606c7e0dc9115
KellenSmith <kellensmith407@gmail.com>
2025-03-22 12:52:42 +0100

    Implemented start and end time utils

--- src/app/(pages)/calendar/[event-id]/event-utils.ts
+++ src/app/(pages)/calendar/[event-id]/event-utils.ts
@@ -1,4 +1,4 @@
-import dayjs, { Dayjs } from "dayjs";
+import dayjs from "dayjs";
 import GlobalConstants from "../../../GlobalConstants";
 
 export const isEventPublished = (event) =>
@@ -9,40 +9,41 @@ export const isUserParticipant = (user: any, event: any) =>
         .map((participant: any) => participant[GlobalConstants.USER_ID])
         .includes(user[GlobalConstants.ID]);
 
-export const sortTasks = (tasks) =>
-    tasks.sort((taska, taskb) => {
-        if (
-            dayjs(taska[GlobalConstants.END_TIME]).isSame(
-                dayjs(taskb[GlobalConstants.END_TIME]),
-                "minute",
-            )
-        ) {
-            if (
-                dayjs(taska[GlobalConstants.START_TIME]).isSame(
-                    dayjs(taskb[GlobalConstants.START_TIME]),
-                    "minute",
-                )
-            )
-                return taska[GlobalConstants.NAME].localeCompare(taskb[GlobalConstants.NAME]);
-            return dayjs(taska[GlobalConstants.START_TIME]).isBefore(
-                dayjs(taskb[GlobalConstants.START_TIME]),
-            );
-        }
-        return dayjs(taska[GlobalConstants.END_TIME]).isBefore(
-            dayjs(taskb[GlobalConstants.END_TIME]),
-        );
-    });
-
 export const isTaskSelected = (task: any, selectedTasks: any[]) =>
     selectedTasks.map((task) => task[GlobalConstants.ID]).includes(task[GlobalConstants.ID]);
 
 export const getEarliestStartTime = (tasks) =>
     tasks
-        .map((task) => dayjs(task[GlobalConstants.START_TIME]))
-        .sort((start1: Dayjs, start2: Dayjs) => start1.isBefore(start2))[0];
+        .map((task) => task[GlobalConstants.START_TIME])
+        .sort((startTime1, startTime2) => dayjs(startTime1).isBefore(dayjs(startTime2)))[0];
+
+export const getEarliestEndTime = (tasks) =>
+    tasks
+        .map((task) => task[GlobalConstants.END_TIME])
+        .sort((startTime1, startTime2) => dayjs(startTime1).isBefore(dayjs(startTime2)))[0];
 
 export const getLatestEndTime = (tasks) =>
     tasks
-        .map((task) => dayjs(task[GlobalConstants.END_TIME]))
-        .sort((start1: Dayjs, start2: Dayjs) => start1.isBefore(start2))
+        .map((task) => task[GlobalConstants.END_TIME])
+        .sort((startTime1, startTime2) => dayjs(startTime1).isBefore(dayjs(startTime2)))
         .at(-1);
+
+export const sortTasks = (tasks) =>
+    tasks.sort((task1, task2) => {
+        if (
+            dayjs(task1[GlobalConstants.START_TIME]).isSame(
+                task2[GlobalConstants.START_TIME],
+                "minute",
+            )
+        )
+            return dayjs(task1[GlobalConstants.START_TIME]).isBefore(
+                dayjs(task2[GlobalConstants.START_TIME]),
+            );
+        if (
+            dayjs(task1[GlobalConstants.END_TIME]).isSame(task2[GlobalConstants.END_TIME], "minute")
+        )
+            return dayjs(task1[GlobalConstants.END_TIME]).isBefore(
+                dayjs(task2[GlobalConstants.END_TIME]),
+            );
+        return task1[GlobalConstants.NAME].localeCompare(task2[GlobalConstants.NAME]);
+    });
