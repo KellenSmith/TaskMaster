c3df801fc0bd90b93636ca75a4b22ab803d659ef
KellenSmith <kellensmith407@gmail.com>
2025-03-22 12:53:04 +0100

    Implemented util for dummy id

--- src/app/ui/utils.ts
+++ src/app/ui/utils.ts
@@ -1,3 +1,12 @@
 import dayjs, { Dayjs } from "dayjs";
+import GlobalConstants from "../GlobalConstants";
 
 export const formatDate = (date: string | Date | Dayjs): string => dayjs(date).format("L HH:mm");
+
+export const getDummyId = (existingItems: any[]) =>
+    existingItems?.length > 0
+        ? existingItems
+              .map((item: any) => item[GlobalConstants.ID])
+              .sort((id1: string, id2: string) => id1.localeCompare(id2))
+              .at(-1) + "+"
+        : "+";
