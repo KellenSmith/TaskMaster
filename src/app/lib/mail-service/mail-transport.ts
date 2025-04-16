820945ecc31e9c7e6a2db01f49aec945338f39cb
KellenSmith <kellensmith407@gmail.com>
2025-04-16 11:25:23 +0200

    Specified type for smtp port

--- src/app/lib/mail-service/mail-transport.ts
+++ src/app/lib/mail-service/mail-transport.ts
@@ -6,7 +6,7 @@ const mailTransport =
     globalMailService.mailTransport ||
     createTransport({
         host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
+        port: parseInt(process.env.SMTP_PORT),
         secure: false, // true for port 465, false for other ports
         auth: {
             user: process.env.EMAIL,
