9b0af49ce2367779f0731a4755e0060bd03093be
KellenSmith <kellensmith407@gmail.com>
2025-09-08 17:48:06 +0200

    fix: Correct date format in formatDate utility function

--- src/app/ui/utils.ts
+++ src/app/ui/utils.ts
@@ -2,7 +2,8 @@ import { Prisma } from "@prisma/client";
 import dayjs, { Dayjs } from "dayjs";
 import Error from "next/error";
 
-export const formatDate = (date: string | Date | Dayjs): string => dayjs(date).format("L HH:mm");
+export const formatDate = (date: string | Date | Dayjs): string =>
+    dayjs(date).format("YYYY/MM/DD HH:mm");
 export const formatPrice = (price: number): number => price / 100;
 
 export const openResourceInNewTab = (resourceUrl: string) => {
