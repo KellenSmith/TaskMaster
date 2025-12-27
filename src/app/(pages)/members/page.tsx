aa47f9729b4c6fb95221bf5066614e512c916994
Kellen <kellensmith407@proton.me>
2025-12-27 13:02:31 +0100

    Move user get actions to use location

--- src/app/(pages)/members/page.tsx
+++ src/app/(pages)/members/page.tsx
@@ -1,14 +1,17 @@
 "use server";
 
-import { getAllUsers, getLoggedInUser } from "../../lib/user-actions";
 import MembersDashboard from "./MembersDashboard";
 import ErrorBoundarySuspense from "../../ui/ErrorBoundarySuspense";
 import { prisma } from "../../../../prisma/prisma-client";
 
 const MembersPage = async () => {
-    const loggedInUser = await getLoggedInUser();
 
-    const membersPromise = getAllUsers(loggedInUser.id);
+    const membersPromise = prisma.user.findMany({
+        include: {
+            user_membership: true,
+            skill_badges: true,
+        },
+    });
     const skillBadgesPromise = prisma.skillBadge.findMany({ include: { user_skill_badges: true } });
 
     // TODO: If on mobile, just show list of pending members, viewable and validatable
