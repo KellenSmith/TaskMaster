4f5f08f48cab3b42d4a887eeea8ba90b5c413476
Kellen <kellensmith407@proton.me>
2026-01-03 15:04:03 +0100

    Fix bug

--- src/app/context/ServerContextWrapper.tsx
+++ src/app/context/ServerContextWrapper.tsx
@@ -3,7 +3,7 @@ import ContextWrapper from "./ContextWrapper";
 import { getOrganizationSettings } from "../lib/organization-settings-actions";
 import { getLoggedInUser } from "../lib/user-actions";
 import { prisma } from "../../../prisma/prisma-client";
-import { UserRole } from "@prisma/client";
+import { Prisma, UserRole } from "@prisma/client";
 import { userHasRolePrivileges } from "../lib/auth/auth-utils";
 
 interface ServerContextWrapperProps {
@@ -11,36 +11,29 @@ interface ServerContextWrapperProps {
 }
 
 const ServerContextWrapper: FC<ServerContextWrapperProps> = async ({ children }) => {
-    const organizationSettingsPromise = getOrganizationSettings();
-
     const loggedInUser = await getLoggedInUser();
-    if (!loggedInUser)
-        return await prisma.infoPage.findMany({
-            where: {
-                lowest_allowed_user_role: null,
-            },
-            include: { titleText: { include: { translations: true } } },
-        });
 
     const allowedUserRolePrivileges = Object.values(UserRole).filter((role) =>
         userHasRolePrivileges(loggedInUser, role),
     ) as UserRole[];
-    // All users are allowed to view pages with no role restrictions
-    allowedUserRolePrivileges.push(null);
 
-    const infoPages = await prisma.infoPage.findMany({
-        where: {
-            lowest_allowed_user_role: { in: allowedUserRolePrivileges }
-
-        },
+    // Pages with no role restrictions are always allowed
+    const lowestAllowedUserRoleCondition: Prisma.InfoPageWhereInput = { OR: [{ lowest_allowed_user_role: { equals: null } }] };
+    if (allowedUserRolePrivileges.length > 0) {
+        lowestAllowedUserRoleCondition.OR.push({ lowest_allowed_user_role: { in: allowedUserRolePrivileges } });
+    }
+    const infoPagesPromise = prisma.infoPage.findMany({
+        where: lowestAllowedUserRoleCondition,
         include: { titleText: { include: { translations: true } } },
     });
+
+    const organizationSettingsPromise = getOrganizationSettings();
 
     return (
         <ContextWrapper
             organizationSettingsPromise={organizationSettingsPromise}
             userPromise={new Promise((resolve) => resolve(loggedInUser))}
-            infoPagesPromise={new Promise((resolve) => resolve(infoPages))}
+            infoPagesPromise={infoPagesPromise}
         >
             {children}
         </ContextWrapper>
