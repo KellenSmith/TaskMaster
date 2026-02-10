b1aaf13046cc0e50db52c0cf6fc5a7b6ea5dd598
Kellen <kellensmith407@proton.me>
2026-02-10 08:16:21 +0100

    Add LanguageTranslations and integrate into TicketDashboard for improved localization

--- src/app/(pages)/ticket/page.tsx
+++ src/app/(pages)/ticket/page.tsx
@@ -1,42 +1,26 @@
-import { Prisma } from "../../../prisma/generated/client";
 import { prisma } from "../../../prisma/prisma-client";
+import ErrorBoundarySuspense from "../../ui/ErrorBoundarySuspense";
+import TicketDashboard from "./TicketDashboard";
 
 interface TicketPageProps {
     searchParams: Promise<{ [eventParticipantId: string]: string }>;
 }
 
 const TicketPage = async ({ searchParams }: TicketPageProps) => {
-    console.log(await searchParams)
     const eventParticipantId = (await searchParams).eventParticipantId;
 
-    let eventParticipant: Prisma.EventParticipantGetPayload<{ include: { ticket: { include: { event: true } }, user: { select: { id: true, nickname: true } } } }>;
-    try {
-        eventParticipant = await prisma.eventParticipant.findUniqueOrThrow({
-            where: {
-                id: eventParticipantId,
-            },
-            include: {
-                ticket: { include: { event: true } },
-                user: { select: { id: true, nickname: true } },
-            }
-        });
-    } catch (error) {
-        return <div>this is not a ticket</div>
-    }
-    const ticket = eventParticipant.ticket;
-    const event = eventParticipant.ticket.event;
-    const user = eventParticipant.user;
-
-
+    const eventParticipantPromise = prisma.eventParticipant.findUnique({
+        where: {
+            id: eventParticipantId,
+        },
+        include: {
+            ticket: { include: { event: true } },
+            user: { select: { id: true, nickname: true } },
+        }
+    });
 
     return (
-        <div>
-            <h1>Ticket Page</h1>
-            <p>This is where you can view your ticket with ID: {eventParticipant.id}.</p>
-            <p>Event: {event.title}</p>
-            <p>User: {user.nickname}</p>
-            <p>Ticket type: {ticket.type}</p>
-        </div>
+        <ErrorBoundarySuspense><TicketDashboard eventParticipantPromise={eventParticipantPromise} /></ErrorBoundarySuspense>
     );
 }
 
