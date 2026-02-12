2f6368ab1694e11e1c7acc95c4beb53e987e7753
Kellen <kellensmith407@proton.me>
2026-02-12 17:10:20 +0100

    Security patch

--- src/app/(pages)/orders/page.tsx
+++ src/app/(pages)/orders/page.tsx
@@ -1,10 +1,9 @@
 "use server";
 import OrdersDashboard from "./OrdersDashboard";
 import { getAllOrders } from "../../lib/order-actions";
-import GlobalConstants from "../../GlobalConstants";
 import ErrorBoundarySuspense from "../../ui/ErrorBoundarySuspense";
 
-const OrdersPage = () => {
+const OrdersPage = async () => {
     const ordersPromise = getAllOrders();
     return (
         <ErrorBoundarySuspense>
