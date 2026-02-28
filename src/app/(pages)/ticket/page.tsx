652351bc1ce5334059c25d615e0ffb84b2aa97b9
Kellen <kellensmith407@proton.me>
2026-02-28 16:20:16 +0100

    Extend TicketPage tests to cover user authorization and event participant retrieval

--- src/app/(pages)/ticket/page.tsx
+++ src/app/(pages)/ticket/page.tsx
@@ -1,4 +1,6 @@
 import { prisma } from "../../../prisma/prisma-client";
+import { getLoggedInUser } from "../../lib/user-helpers";
+import { isMembershipExpired, isUserAdmin } from "../../lib/utils";
 import TicketDashboard from "./TicketDashboard";
 
 interface TicketPageProps {
@@ -6,19 +8,46 @@ interface TicketPageProps {
 }
 
 const TicketPage = async ({ searchParams }: TicketPageProps) => {
+    const loggedInUser = await getLoggedInUser();
+    if (isMembershipExpired(loggedInUser)) throw new Error("Unauthorized");
+
     const eventParticipantId = (await searchParams).eventParticipantId;
 
-    const eventParticipantPromise = prisma.eventParticipant.findUnique({
+    const eventParticipant = await prisma.eventParticipant.findUnique({
         where: {
             id: eventParticipantId,
         },
         include: {
-            ticket: { include: { event: true } },
+            ticket: {
+                include: {
+                    event: {
+                        include: {
+                            tasks: {
+                                where: {
+                                    assignee_id: loggedInUser!.id,
+                                },
+                                select: {
+                                    id: true,
+                                },
+                            },
+                        },
+                    },
+                },
+            },
             user: { select: { id: true, nickname: true } },
         },
     });
 
-    return <TicketDashboard eventParticipantPromise={eventParticipantPromise} />;
+    if (eventParticipant) {
+        const isEventHost = eventParticipant.ticket.event.host_id === loggedInUser!.id;
+        const isVolunteer = eventParticipant.ticket.event.tasks.length || 0 > 0;
+        const isOwnTicket = eventParticipant.user_id === loggedInUser!.id;
+
+        if (!(isUserAdmin(loggedInUser) || isEventHost || isVolunteer || isOwnTicket))
+            throw new Error("Unauthorized");
+    }
+
+    return <TicketDashboard eventParticipant={eventParticipant} />;
 };
 
 export default TicketPage;
